77a99f2b46d9e913f244f95a05de9064354a41cd
Øystein Håberg <islandstone89@gmail.com>
2024-09-21 22:24:27 +0200

    * Added and styled overlay * Added JS functionality for opening and closening menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,21 @@
 
-console.log("Test");
+/* SELECTORS */
+
+let openMenuButton = document.querySelector(".js-navbar__menu__open");
+let closeMenuButton = document.querySelector(".js-navbar__menu__close");
+
+
+/* FUNCTIONS */
+
+function openMenu() {
+    openMenuButton.setAttribute("aria-expanded", "true")
+}
+
+function closeMenu() {
+    openMenuButton.setAttribute("aria-expanded", "false")
+}
+
+/* EVENT LISTENERS */
+
+openMenuButton.addEventListener("click", openMenu);
+closeMenuButton.addEventListener("click", closeMenu);
